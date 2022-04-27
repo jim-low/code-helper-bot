959e051d635c1a93027b21532c2bd9fe4958bd87
jim-low <robil12380@gmail.com>
2022-04-27 14:55:45 +0800

    feat: add custom slash commands

--- main.ts
+++ main.ts
@@ -8,9 +8,82 @@ const client = new DiscordJS.Client({
         Intents.FLAGS.GUILD_MESSAGES,
     ]
 })
+const programmingLanguages = ['C', 'C++', 'Javascript', 'Typescript', 'Java', 'Python', 'Lua', 'PHP']
 
 client.on('ready', () => {
     console.log('bot is ready!!')
+
+    // guild - recommended for development
+    const guildID = process.env.GUILD_ID ?? ''
+    const guild = client.guilds.cache.get(guildID)
+    const commands = guild ? guild.commands : client.application?.commands
+
+    commands?.create({
+        name: 'ping',
+        description: 'Replies with pong.',
+    })
+
+    commands?.create({
+        name: 'add',
+        description: 'Adds two numbers.',
+        options: [
+            {
+                name: 'num1',
+                description: 'First Number',
+                required: true,
+                type: DiscordJS.Constants.ApplicationCommandOptionTypes.NUMBER
+            },
+            {
+                name: 'num2',
+                description: 'Second Number',
+                required: true,
+                type: DiscordJS.Constants.ApplicationCommandOptionTypes.NUMBER
+            },
+        ]
+    })
+
+    commands?.create({
+        name: 'languages',
+        description: 'List Known Programming Languages.',
+    })
+    // global - recommended for publishing
+})
+
+client.on('interactionCreate', async (interaction) => {
+    if (!interaction.isCommand()) {
+        return
+    }
+
+    const { commandName, options } = interaction
+
+    if (commandName === 'ping') {
+        interaction.reply({
+            content: 'pong',
+            ephemeral: true
+        })
+    }
+
+    if (commandName === 'add') {
+        const num1 = options.getNumber('num1') ?? 0
+        const num2 = options.getNumber('num2') ?? 0
+
+        await interaction.deferReply({
+            ephemeral: false
+        })
+
+        await new Promise(resolve => setTimeout(resolve, 5000))
+
+        interaction.editReply({
+            content: `The sum of ${num1} and ${num2} is ${num1 + num2}`,
+            // ephemeral: true
+        })
+    }
+
+    if (commandName === 'languages') {
+        interaction.reply({
+            content: `Here are your list of languages: ${programmingLanguages.join(', ')}`
+        })
+    }
 })
 
 client.on('messageCreate', (message) => {
@@ -26,9 +99,9 @@ client.on('messageCreate', (message) => {
         })
     }
 
-    if (message.content.toLowerCase().includes('jimmus')) {
+    if (message.content.toLowerCase().includes('programming language')) {
         message.reply({
-            content: 'it is Jim, not jimmus'
+            content: programmingLanguages[Math.floor(Math.random() * programmingLanguages.length)]
         })
     }
 })
